124b1f4674b7f4c6d84014a11063a6cf97da418f
N00byEdge <me@hannesbredberg.se>
2018-07-15 19:20:11 +0200

    Added more tests, fixed projects

--- Tests/Combat.cpp
+++ Tests/Combat.cpp
@@ -19,29 +19,9 @@ TEST(Combat, MarineTwoOnOne) {
 
     fap.simulate(-1);
 
+    // Two marines beat one
     EXPECT_FALSE(fap.getState().first->empty());
     EXPECT_TRUE(fap.getState().second->empty());
-  }
-
-  {
-    FAP::FastAPproximation fap;
-
-    
-    fap.addUnitPlayer1(
-      testUnit(BWAPI::UnitTypes::Terran_Marine)
-      .setPosition({ 0, 0 })
-    );
-
-    for (int i = 0; i < 2; ++i)
-      fap.addUnitPlayer2(
-        testUnit(BWAPI::UnitTypes::Terran_Marine)
-        .setPosition({ 0, 0 })
-      );
-
-    fap.simulate(-1);
-
-    EXPECT_TRUE(fap.getState().first->empty());
-    EXPECT_FALSE(fap.getState().second->empty());
   }
 }
 
@@ -60,6 +40,46 @@ TEST(Combat, MarineOnZealot) {
 
   fap.simulate(-1);
 
+  // Zealot beats marine
   EXPECT_FALSE(fap.getState().first->empty());
   EXPECT_TRUE(fap.getState().second->empty());
+}
+
+TEST(Combat, MarineOnZergling) {
+  FAP::FastAPproximation fap;
+
+  fap.addUnitPlayer1(
+    testUnit(BWAPI::UnitTypes::Zerg_Zergling)
+    .setPosition({ 0, 0 })
+  );
+
+  fap.addUnitPlayer2(
+    testUnit(BWAPI::UnitTypes::Terran_Marine)
+    .setPosition({ 0, 0 })
+  );
+
+  fap.simulate(-1);
+
+  // Zerling beats marine
+  EXPECT_FALSE(fap.getState().first->empty());
+  EXPECT_TRUE(fap.getState().second->empty());
+}
+
+TEST(Combat, MarineFiringAtValkyrie) {
+  FAP::FastAPproximation fap;
+
+  fap.addUnitPlayer1(
+    testUnit(BWAPI::UnitTypes::Terran_Marine)
+    .setPosition({ 0, 0 })
+  );
+
+  fap.addUnitPlayer2(
+    testUnit(BWAPI::UnitTypes::Terran_Valkyrie)
+    .setPosition({ 0, 0 })
+  );
+
+  fap.simulate(1);
+
+  // Marine does 6-2 damage to valkyrie
+  EXPECT_EQ(fap.getState().second->front().health, fap.getState().second->front().maxHealth - (4 << 8));
 }
--- Tests/DataLoad.cpp
+++ Tests/DataLoad.cpp
@@ -93,3 +93,62 @@ TEST(Data, EmptyBunker) {
 
   EXPECT_EQ(bunker.armor, 1);
 }
+
+TEST(Data, BunkerWithAttackUpgrade) {
+  FAP::FastAPproximation fap;
+
+  fap.addUnitPlayer1(
+    FAP::makeUnit()
+    .setUnitType(BWAPI::UnitTypes::Terran_Bunker)
+    .setPosition({ 0, 0 })
+    .setHealth(BWAPI::UnitTypes::Terran_Bunker.maxHitPoints())
+    .setShields(BWAPI::UnitTypes::Terran_Bunker.maxShields())
+    .setFlying(false)
+    .setElevation(-1)
+    .setScore(5)
+    .setAttackerCount(1)
+    .setArmorUpgrades(0)
+    .setAttackUpgrades(1)
+    .setShieldUpgrades(0)
+    .setSpeedUpgrade(false)
+    .setAttackSpeedUpgrade(false)
+    .setAttackCooldownRemaining(0)
+    .setStimmed(false)
+    .setRangeUpgrade(false)
+  );
+
+  auto bunker = fap.getState().first->front();
+  EXPECT_EQ(bunker.groundDamage, BWAPI::UnitTypes::Terran_Marine.groundWeapon().damageAmount() + 1);
+  EXPECT_EQ(bunker.airDamage, BWAPI::UnitTypes::Terran_Marine.airWeapon().damageAmount() + 1);
+}
+
+TEST(Data, BunkerWithRangeUpgrade) {
+  FAP::FastAPproximation fap;
+
+  fap.addUnitPlayer1(
+    FAP::makeUnit()
+    .setUnitType(BWAPI::UnitTypes::Terran_Bunker)
+    .setPosition({ 0, 0 })
+    .setHealth(BWAPI::UnitTypes::Terran_Bunker.maxHitPoints())
+    .setShields(BWAPI::UnitTypes::Terran_Bunker.maxShields())
+    .setFlying(false)
+    .setElevation(-1)
+    .setScore(5)
+    .setAttackerCount(1)
+    .setArmorUpgrades(0)
+    .setAttackUpgrades(0)
+    .setShieldUpgrades(0)
+    .setSpeedUpgrade(false)
+    .setAttackSpeedUpgrade(false)
+    .setAttackCooldownRemaining(0)
+    .setStimmed(false)
+    .setRangeUpgrade(true)
+  );
+
+  auto bunker = fap.getState().first->front();
+
+  EXPECT_EQ(bunker.groundMaxRangeSquared,
+    (BWAPI::UnitTypes::Terran_Marine.groundWeapon().maxRange() + 64) * (BWAPI::UnitTypes::Terran_Marine.groundWeapon().maxRange() + 64));
+  EXPECT_EQ(bunker.airMaxRangeSquared,
+    (BWAPI::UnitTypes::Terran_Marine.airWeapon().maxRange() + 64) * (BWAPI::UnitTypes::Terran_Marine.airWeapon().maxRange() + 64));
+}
